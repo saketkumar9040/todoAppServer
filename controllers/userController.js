12040a4dc681c98385aa7346f866c44664d65ff0
saket kumar <saketk89@gmail.com>
2023-04-21 15:37:14 +0530

    change in fs

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,7 +33,7 @@ export const register = async (req, res) => {
         console.log(err);
       });
 
-     fs.rmSync("./tmp/images",{recursive:true});
+     fs.rmSync("./temperoryImages",{recursive:true});
 
     userExists = await User.create({
       name,

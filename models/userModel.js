7cf6f569a9ae83edf62a232e54ba83cfa6b1c3f5
saket kumar <saketk89@gmail.com>
2023-04-28 19:12:28 +0530

    remover required in password of userModel

--- models/userModel.js
+++ models/userModel.js
@@ -19,8 +19,6 @@ const userSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: true,
-    minLength: [8, "password must be at least 8 characters long"],
     select: false,
   },
   avatar: {
